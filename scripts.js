55333a349e7a6808c8777903b585b00b42b1f32d
Matheus Munarão <contatomatheusmunarao@gmail.com>
2021-05-17 18:43:04 -0300

    completion of the base project

--- scripts.js
+++ scripts.js
@@ -1,63 +1,41 @@
 const Modal = {
   open() {
-    // Abrir Modal
-    // Adicionar a class active ao modal
     document.querySelector(".modal-overlay").classList.add("active");
   },
   close() {
-    // Fechar o modal
-    // Remover a class active do modal
     document.querySelector(".modal-overlay").classList.remove("active");
   },
 };
 
-/* 
-    Acima montamos um OBJETO
-    e dentro deste OBJETO possuímos duas funcionalidades/propriedades.
-
-    Quando uma FUNÇÃO está dentro de um OBJETO, ela recebe o nome de MÉTODO.
-
-
-    DOM = Document Object Model (Modelagem do HTML para o JavaScript)
-
-    DOM é o modelo de toda a estrutura HTML passada para o JavaScript
-    DOM (document) também é um OBJETO e como todo OBJETO, ele possui propriedades e funcionalidades
-
-
-    o . vai encadeando uma coisa a outra, ligando os elementos.
-*/
-
-const transactions = [
-  {
-    id: 1,
-    description: "Luz",
-    amount: -50000,
-    date: "23/01/2021",
+const Storage = {
+  get() {
+    return JSON.parse(localStorage.getItem("dev.finances:transactions")) || [];
   },
-  {
-    id: 2,
-    description: "Website",
-    amount: 500000,
-    date: "23/01/2021",
+  set(transactions) {
+    localStorage.setItem(
+      "dev.finances:transaction",
+      JSON.stringify(transactions)
+    );
   },
-  {
-    id: 3,
-    description: "Internet",
-    amount: -20000,
-    date: "23/01/2021",
-  },
-];
+};
 
 const Transaction = {
-  all: transactions,
+  all: Storage.get(),
+
+  add(transaction) {
+    Transaction.all.push(transaction);
+
+    App.reload();
+  },
+  remove(index) {
+    Transaction.all.splice(index, 1);
+
+    App.reload();
+  },
   incomes() {
     let income = 0;
-    // pegar todas as transações
-    // para cada transação,
     Transaction.all.forEach((transaction) => {
-      // se ela for maior que zero
       if (transaction.amount > 0) {
-        // somar a uma variavel e retornar a variavel
         income += transaction.amount;
       }
     });
@@ -65,12 +43,8 @@ const Transaction = {
   },
   expenses() {
     let expense = 0;
-    // pegar todas as transações
-    // para cada transação,
     Transaction.all.forEach((transaction) => {
-      // se ela for menor que zero
       if (transaction.amount < 0) {
-        // somar a uma variavel e retornar a variavel
         expense += transaction.amount;
       }
     });
@@ -86,28 +60,24 @@ const DOM = {
 
   addTransaction(transaction, index) {
     const tr = document.createElement("tr");
-    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+    tr.dataset.index = index;
 
     DOM.transactionsContainer.appendChild(tr);
   },
-
-  innerHTMLTransaction(transaction) {
+  innerHTMLTransaction(transaction, index) {
     const CSSclass = transaction.amount > 0 ? "income" : "expense";
-
     const amount = Utils.formatCurrency(transaction.amount);
-
     const html = `
             <td class="description">${transaction.description}</td>
             <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
-                <img src="./assets/minus.svg" alt="Remover Transação">
+                <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover Transação">
             </td>
         `;
-
     return html;
   },
-
   updateBalance() {
     document.getElementById("incomeDisplay").innerHTML = Utils.formatCurrency(
       Transaction.incomes()
@@ -119,16 +89,27 @@ const DOM = {
       Transaction.total()
     );
   },
+  clearTransactions() {
+    DOM.transactionsContainer.innerHTML = "";
+  },
 };
 
 const Utils = {
+  formatAmount(value) {
+    value = value * 100;
+
+    return Math.round(value);
+  },
+  formatDate(date) {
+    const splittedDate = date.split("-");
+
+    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
+  },
   formatCurrency(value) {
     const signal = Number(value) < 0 ? "-" : "";
 
     value = String(value).replace(/\D/g, "");
-
     value = Number(value) / 100;
-
     value = value.toLocaleString("pt-BR", {
       style: "currency",
       currency: "BRL",
@@ -138,10 +119,78 @@ const Utils = {
   },
 };
 
-transactions.forEach(function (transaction) {
-  DOM.addTransaction(transaction);
-});
+const Form = {
+  description: document.querySelector("input#description"),
+  amount: document.querySelector("input#amount"),
+  date: document.querySelector("input#date"),
 
-DOM.updateBalance();
+  getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value,
+    };
+  },
+  validateFields() {
+    const { description, amount, date } = Form.getValues();
 
-/* Uma função para jogar fora seus elementos internos, necessita do return */
+    if (
+      description.trim() === "" ||
+      amount.trim() === "" ||
+      date.trim() === ""
+    ) {
+      throw new Error("Por favor, preencha todos os campos.");
+    }
+  },
+  formatValues() {
+    let { description, amount, date } = Form.getValues();
+
+    amount = Utils.formatAmount(amount);
+    date = Utils.formatDate(date);
+
+    return {
+      description,
+      amount,
+      date,
+    };
+  },
+  saveTransaction(transaction) {
+    Transaction.add(transaction);
+  },
+  clearFields() {
+    Form.description.value = "";
+    Form.amount.value = "";
+    Form.date.value = "";
+  },
+
+  submit(event) {
+    event.preventDefault();
+
+    try {
+      Form.validateFields();
+      const transaction = Form.formatValues();
+      Transaction.add(transaction);
+      Form.clearFields();
+      Modal.close();
+    } catch (error) {
+      alert(error.message);
+    }
+  },
+};
+
+const App = {
+  init() {
+    Transaction.all.forEach((transaction, index) => {
+      DOM.addTransaction(transaction, index);
+    });
+    DOM.updateBalance();
+
+    Storage.set(Transaction.all);
+  },
+  reload() {
+    DOM.clearTransactions();
+    App.init();
+  },
+};
+
+App.init();
